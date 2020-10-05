5aa8481212f7a01bab40726615ed0bd3defa1259
Bas van den Wijngaard <bas@mightymedia.nl>
2020-10-05 22:45:48 +0200

    Add min and max value options for number validator (issue #14)

--- src/js/validators/number.js
+++ src/js/validators/number.js
@@ -7,14 +7,50 @@ mightyFormValidator.validators.add('number', {
 		mightyFormValidator.utilities.log('Function: number.validate()');
 
 		var isValid,
-			value = self.value;
+			value = self.value,
+			minValue = null,
+			maxValue = null;
 
 		if (typeof options === 'undefined') {
 			options = {};
 		}
 		mightyFormValidator.utilities.log(options);
+		
+		if (typeof options.min !== 'undefined') {
+			//mightyFormValidator.utilities.log('options min');
+			minValue = parseFloat(options.min);
+			mightyFormValidator.utilities.log(minValue);
+
+			if (typeof minValue !== 'number') {
+				minValue = null;
+			}
+			mightyFormValidator.utilities.log(minValue);
+		}
+
+		if (typeof options.max !== 'undefined') {
+			//mightyFormValidator.utilities.log('options max');
+			maxValue = parseFloat(options.max);
+			mightyFormValidator.utilities.log(maxValue);
+
+			if (typeof maxValue !== 'number') {
+				maxValue = null;
+			}
+			mightyFormValidator.utilities.log(maxValue);
+		}
 
 		if (!isNaN(parseFloat(value)) && isFinite(value)) {
+			isValid = true;
+		} else {
+			isValid = false;
+		}
+		
+		if (isValid === true && (minValue !== null && parseFloat(value) >= minValue)) {
+			isValid = true;
+		} else {
+			isValid = false;
+		}
+		
+		if (isValid === true && (maxValue !== null && parseFloat(value) <= maxValue)) {
 			isValid = true;
 		} else {
 			isValid = false;
